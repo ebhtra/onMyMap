7f975cde21592705685f89752af922701087d7ce
ethan <dirtdirtdirtdirt@aol.com>
2015-08-04 16:54:54 -0400

    Git the ball rolling for On the Map. Create initial VC and UI.

---
+++ LoginViewController.swift
@@ -0,0 +1,9 @@
+//
+//  LoginViewController.swift
+//  
+//
+//  Created by Ethan Haley on 8/4/15.
+//
+//
+
+import Foundation
---
+++ On the Map/LoginViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.swift
+//  On the Map
+//
+//  Created by Ethan Haley on 8/4/15.
+//  Copyright (c) 2015 Ethan Haley. All rights reserved.
+//
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+}
